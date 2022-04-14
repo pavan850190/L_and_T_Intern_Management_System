dfeff11e1a7eecc1f6558d7e35f67e617af9d21e
pavan850190 <68550769+pavan850190@users.noreply.github.com>
2022-04-14 17:09:22 +0530

    Update test_main.py

--- 3_Implementation/src/test_main.py
+++ 3_Implementation/src/test_main.py
@@ -1,14 +1,15 @@
 import pytest
 import csv
-
+"""imported csv file"""
 res_file = open("intern.csv", "r")
 list_of_lists = [(line.strip()) for line in res_file]
-
+"""splited csv file data into list"""
 
 @pytest.fixture
 def expected_data1():
     expected_data = list_of_lists[0]
     return expected_data
+    """adding fist person data """
     res_file.close()
 
 
@@ -22,6 +23,7 @@ def expected_data2():
 @pytest.fixture
 def input_data1():
     return '99007476,pavan,22,pavan@gmail.com,123456'
+    """ input data by user for testing """
 
 
 @pytest.fixture
@@ -31,6 +33,7 @@ def input_data2():
 
 def test1(input_data1, expected_data1):
     assert expected_data1 == input_data1
+    """testing the data"""
 
 
 def test2(input_data2, expected_data2):
